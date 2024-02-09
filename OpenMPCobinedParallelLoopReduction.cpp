c596008bfd62ec1970bff8c2e0c4510e6f955896
Suraj Raghuvanshi <92632170+Imsurajr@users.noreply.github.com>
2024-02-09 12:29:32 +0530

    Create OpenMPCobinedParallelLoopReduction.cpp

---
+++ OpenMPCobinedParallelLoopReduction.cpp
@@ -0,0 +1,17 @@
+//Suraj Raghuvanshi : OpenMP Cobined Parallel Loop Reduction
+#include <stdio.h>
+#include <omp.h>
+
+int main() {
+    const int N = 10;
+    int i, sum = 0;
+
+    #pragma omp parallel for reduction(+:sum)
+    for (i = 0; i < N; ++i) {
+        sum += i;
+    }
+
+    printf("Sum of numbers from 0 to %d is: %d\n", N - 1, sum);
+
+    return 0;
+}
